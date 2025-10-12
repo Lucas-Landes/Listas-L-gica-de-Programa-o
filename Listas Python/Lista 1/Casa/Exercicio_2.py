ec2eef03352c7ee1b16c410657ebaba1a0bc05d5
Lucas-Landes <lucaslxm16@gmail.com>
2025-10-12 01:24:15 -0300

    Add files via upload

---
+++ Listas Python/Lista 1/Casa/Exercicio_2.py
@@ -0,0 +1,3 @@
+D=float(input("Qual o valor de dólares desejados para fazer a conversão cambial: "))
+R=D*2.4
+print("A quantidade de Reais para para essa quantia desejada é de: ", R)
---
+++ Listas Python/Lista 1/Casa/Exercicio_3.py
@@ -0,0 +1,1 @@
+AP=float(input("Qual a altura da parede"))
