75d345645231ca23933cd3e76d991c899f6b246c
Lucas-Landes <lucaslxm16@gmail.com>
2025-10-29 01:16:27 -0300

    Delete Listas Python/Lista 3/Exercicio_9.py

--- Listas Python/Lista 3/Exercicio_9.py
+++
@@ -1,9 +0,0 @@
-N=int(input("Digite uma base para potência: "))
-M=int(input("Digite um expoente para potência: "))
-base=N
-potencia=1
-i=1
-while i<=M:
-    potencia=base*N
-    base=potencia
-print(potencia)
